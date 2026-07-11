c1fd3f8e07a07ffb4796eaf8f9396c3732e3fec1
soumojit-D48 <soumojitdas107@gmail.com>
2026-07-11 23:48:57 +0530

    feat: API dashboard routes, static serving, and eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,25 @@
 import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import react from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
 
-export default tseslint.config(js.configs.recommended, ...tseslint.configs.recommended, {
-  ignores: ['**/node_modules/**', '**/dist/**'],
-});
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['**/node_modules/**', '**/dist/**'],
+  },
+  {
+    files: ['**/*.{tsx,jsx}'],
+    plugins: {
+      react,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    },
+    settings: {
+      react: { version: 'detect' },
+    },
+  },
+);
